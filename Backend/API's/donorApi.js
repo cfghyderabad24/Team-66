0b88920e7cde182d3e0cec225a75cebfe5dfecc1
prashanth316 <kpreddy091@gmail.com>
2024-06-23 02:26:20 +0530

    prashanth_changes_donorController

--- Backend/API's/donorApi.js
+++ Backend/API's/donorApi.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const donorApp = express.Router();
 
-import { payment } from '../Controllers/donorController.js';
+import { payment,verifyPayment} from '../Controllers/donorController.js';
+
 
 
 donorApp.post('/payment',payment);
+donorApp.post('/verify',verifyPayment);
 
 module.exports = donorApp;
