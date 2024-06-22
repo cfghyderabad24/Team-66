1ab517f8a7635b1ac838b1f76ad6d9389bcb22e9
veereshk20 <pandiriveereshkumar@gmail.com>
2024-06-23 03:22:34 +0530

    veeresh created announcements scrolling text

--- Frontend/reactapp/src/components/Announcements.jsx
+++ Frontend/reactapp/src/components/Announcements.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Announcements() {
   return (
-    <div>
-      announcements
+    <div style = {{display:'flex'}}>
+        <div style= {{backgroundColor:'orange',height:'40px',margin:'0px'}}>
+            <h3 style={{margin:'10px'}}>Announcements</h3>
+        </div>
+        <marquee direction="left" style={{margin:'10px'}}>
+            <a href="/announcements" style={{color:'black',textDecoration:'none'}}>Awareness programme on 24th July</a>         
+        </marquee>
     </div>
   )
 }
